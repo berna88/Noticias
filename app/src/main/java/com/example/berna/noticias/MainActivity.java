5d6df13143dd3f0f9b47e65ed380f3d7b03ffdbf
Bernardo <berna_halo@hotmail.com>
2017-12-17 16:31:04 -0600

    Ajustes

--- app/src/main/java/com/example/berna/noticias/MainActivity.java
+++ app/src/main/java/com/example/berna/noticias/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.berna.noticias;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tv_info;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_info = (TextView) findViewById(R.id.tv_info);
+        if (getIntent().getExtras()!= null){
+            for (String key:getIntent().getExtras().keySet()){
+                    String value = getIntent().getExtras().getString(key);
+                    tv_info.append(key+" : "+ value);
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/berna/noticias/MiFirebaseMessagingService.java
+++ app/src/main/java/com/example/berna/noticias/MiFirebaseMessagingService.java
@@ -2,6 +2,12 @@ package com.example.berna.noticias;
 
 
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationCompatBase;
 import android.support.v4.app.NotificationCompatExtras;
@@ -28,7 +34,20 @@ public class MiFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     private void mostrarNotificacion(String title, String body) {
+        Intent i = new Intent(this, MainActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),0,i,PendingIntent.FLAG_ONE_SHOT);
 
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setSound(uri)
+                .setContentIntent(pendingIntent);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, builder.build());
     }
 
 
