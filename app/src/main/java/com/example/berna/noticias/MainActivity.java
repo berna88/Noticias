286bd204094f94b18b9e0ad286bf1b440201d489
berna <bhernandez@consistent.com.mx>
2017-12-21 12:34:07 -0600

    Ajuste a diseño de enviar mensaje

--- app/src/main/java/com/example/berna/noticias/MainActivity.java
+++ app/src/main/java/com/example/berna/noticias/MainActivity.java
@@ -1,25 +1,36 @@
 package com.example.berna.noticias;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
     private TextView tv_info;
     private WebView wv_content;
+    private LinearLayout linearLayout;
+    private ImageView iv_get_notification;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        wv_content = (WebView) findViewById(R.id.wv_content);
         tv_info = (TextView) findViewById(R.id.tv_info);
+        iv_get_notification = (ImageView) findViewById(R.id.iv_get_notification);
         String title = getIntent().getStringExtra("title");
         String body = getIntent().getStringExtra("body");
-        String image = getIntent().getStringExtra("image");
-        String url = getIntent().getStringExtra("url");
+        final String image = getIntent().getStringExtra("image");
+        final String url = getIntent().getStringExtra("url");
+        loadImage(image, url);
         //tv_info.append(title+body+url);
         /*
         if (getIntent().getExtras()!= null){
@@ -28,17 +39,30 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         */
-        wv_content.loadDataWithBaseURL(null,"<html><head></head><body><table style=\"width:100%; height:100%;\"><tr><td style=\"vertical-align:middle;\"><img src=\"" + image + "\"></td></tr></table></body></html>", "html/css", "utf-8", null);
-        WebSettings webSettings = wv_content.getSettings();
-        webSettings.setUseWideViewPort(true);
-        webSettings.setLoadWithOverviewMode(true);
-        wv_content.getSettings().setBuiltInZoomControls(true);
-        //wv_content.loadUrl(image+"=s0");
-        wv_content.setWebViewClient(
-                new WebViewClient()
-        );
+        tv_info.setText(title);
 
+    }
+    public void loadImage(String image, final String url){
+        Picasso.with(this).load(image).placeholder(R.drawable.consistent)
+                .error(R.drawable.consistent)
+                .into(iv_get_notification, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        iv_get_notification.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent view = new Intent();
+                                view.setAction(Intent.ACTION_VIEW);
+                                view.setData(Uri.parse(url));
+                                startActivity(view);
+                            }
+                        });
+                    }
 
+                    @Override
+                    public void onError() {
 
+                    }
+                });
     }
 }
